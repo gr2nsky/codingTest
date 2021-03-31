d1cc883fc6f4d5f9b057e513395dce4718e3e266
gr2nsky <44865986+gr2nsky@users.noreply.github.com>
2021-03-31 17:53:12 +0900

    grrensky
    .

--- StringToInt.swift
+++ StringToInt.swift
@@ -14,7 +14,7 @@ print(Int(a))
 /**
  - Parameter s : 1~5길이의 문자열이고 맨 앞에 부호 (+, -)가 올 수 있습니다. 부호 및 숫자루면 이루어지고, 0으로 시작하지 않습니다.
  문자열 s를 받아 숫자로 변환합니다.
- 
+
  문자열을 받아 정수로 변환해 출력합니다.
  */
 func solution(_ s:String) -> Int {
@@ -22,7 +22,7 @@ func solution(_ s:String) -> Int {
     var sign = 1
     //문자열을 배열연산을 할 수 있도록 character배열로 변환
     var chars: [Character] = Array(s)
-    
+
     //부호가 존재한다면, 해당 부호값을 저장(+: 1, -: -1)하고 배열에서 제외
     if chars[0] == "+"{
         chars[0] = "0"
@@ -34,3 +34,9 @@ func solution(_ s:String) -> Int {
     //출력
     return Int(String(chars))!*sign
 }
+
+
+/*
+스위프트는 부호까지 형변환이 가능하다...!!
+return String(s)
+*/
